d60d456f2a06f7ecdbfa21d039e64c5f7b76f1ca
MrSyDoo <166733395+MrSyDoo@users.noreply.github.com>
2024-12-13 11:34:07 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,5 +1,7 @@
-from pyrogram import Client, filters 
+from pyrogram import Client, filters, enums
 from helper.database import madflixbotz
+
+db = madflixbotz
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
@@ -46,7 +48,144 @@ async def addthumbs(client, message):
     await mkn.edit("**Thumbnail Saved Successfully ✅️**")
 
 
+@Client.on_message(filters.private & filters.command('set_prefix'))
+async def add_caption(client, message):
 
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Roofiverse`**")
+    prefix = message.text.split(" ", 1)[1]
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_prefix(message.from_user.id, prefix)
+    await SnowDev.edit("__**✅ ᴘʀᴇꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('del_prefix'))
+async def delete_prefix(client, message):
+
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await db.get_prefix(message.from_user.id)
+    if not prefix:
+        return await SnowDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await db.set_prefix(message.from_user.id, None)
+    await SnowDev.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('see_prefix'))
+async def see_caption(client, message):
+
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await db.get_prefix(message.from_user.id)
+    if prefix:
+        await SnowDev.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{prefix}`")
+    else:
+        await SnowDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+
+
+# SUFFIX
+@Client.on_message(filters.private & filters.command('set_suffix'))
+async def add_csuffix(client, message):
+
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Suffix__\n\nExᴀᴍᴩʟᴇ:- `/set_suffix @Roofiverse`**")
+    suffix = message.text.split(" ", 1)[1]
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_suffix(message.from_user.id, suffix)
+    await SnowDev.edit("__**✅ ꜱᴜꜰꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('del_suffix'))
+async def delete_suffix(client, message):
+
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await db.get_suffix(message.from_user.id)
+    if not suffix:
+        return await SnowDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+    await db.set_suffix(message.from_user.id, None)
+    await SnowDev.edit("__**❌️ ꜱᴜꜰꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('see_suffix'))
+async def see_csuffix(client, message):
+
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await db.get_suffix(message.from_user.id)
+    if suffix:
+        await SnowDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
+    else:
+        await SnowDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+        
+@Client.on_message(filters.private & filters.command('set_dump'))
+async def add_dump(client, message):
+
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Roofiverse`**")
+    dump = message.text.split(" ", 1)[1]
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_dump(message.from_user.id, dump)
+    await SyD.edit("__**✅ ᴘʀᴇꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('del_dump'))
+async def delete_dump(client, message):
+
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    dump = await db.get_dump(message.from_user.id)
+    if not dump:
+        return await SyD.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await db.set_dump(message.from_user.id, message.from_user.id)
+    await SyD.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('see_from'))
+async def see_csuffix(client, message):
+
+    SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await db.get_syd(message.from_user.id)
+    if suffix:
+        await SnowDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
+    else:
+        await SnowDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+        
+@Client.on_message(filters.private & filters.command('set_from'))
+async def add_dump(client, message):
+
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Roofiverse`**")
+    frm = message.text.split(" ", 1)[1]
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_syd(message.from_user.id, frm)
+    await SyD.edit("__**✅ ꜱᴀᴠᴇᴅ**__")
+
+
+@Client.on_message(filters.private & filters.command('del_from'))
+async def delete_dump(client, message):
+
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    dump = await db.get_dump(message.from_user.id)
+    if not dump:
+        return await SyD.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await db.set_syd(message.from_user.id, message.from_user.id)
+    await SyD.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('see_dump'))
+async def see_dump(client, message):
+
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    dump = await db.get_dump(message.from_user.id)
+    if dump:
+        await SyD.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{dump}`")
+    else:
+        await SyD.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+
+@Client.on_message(filters.private & filters.command('set_dump'))
+async def add_dump(client, message):
+
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Roofiverse`**")
+    dump = message.text.split(" ", 1)[1]
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_dump(message.from_user.id, dump)
+    await SyD.edit("__**✅ ꜱᴀᴠᴇᴅ**__")
 
 
 # Jishu Developer 
