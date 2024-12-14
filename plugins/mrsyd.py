840ca611db0f3f1ab65a24efd1ee307e732fede7
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-12-14 19:58:50 +0530

    Update mrsyd.py

--- plugins/mrsyd.py
+++ plugins/mrsyd.py
@@ -253,7 +253,7 @@ async def autosyd(client, file_details):
     # Remove matching patterns from 'sydd'
     for pattern in patterns:
         if pattern.match(sydd):
-            syddtg = sydd.replace(pattern.pattern, "")
+            sydd = sydd.replace(pattern.pattern, "")
     print(f"Original File Name: {file_name}")
 
 
@@ -269,7 +269,7 @@ async def autosyd(client, file_details):
     
     if episode_number and season_no:
         formatted_episode = f"S{int(season_no):02d}E{int(episode_number):02d} "
-        Syd = formatted_episode + syddtg
+        Syd = formatted_episode + sydd
         mrsyds = ['YTS.MX', 'SH3LBY', 'Telly', 'Moviez', 'NazzY', 'VisTa', 'PiRO', 'PAHE', 'ink', 'mkvcinemas', 'CZ', 'WADU', 'PrimeFix', 'HDA', 'PSA', 'GalaxyRG', '-Bigil', 'TR', 'www.', '@',
             '-TR', '-SH3LBY', '-Telly', '-NazzY', '-PAHE', '-WADU', 'MoviezVerse', 't3nzin', '[Tips', 'Eac3', '(@'
                  ]
