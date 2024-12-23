2d720159b43f62861c292a1c398fd898d7fceacb
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-12-23 20:05:08 +0530

    Update mrsyd.py

--- plugins/mrsyd.py
+++ plugins/mrsyd.py
@@ -271,10 +271,10 @@ async def autosyd(client, file_details):
         syd_tg = int(episode_number)
         syd_xyz = int(season_no)
         tg_Syd_Xyz = file_details['season']
-        if tg_Syd_Xyz == "True":
-            formatted_episode = f"S{syd_xyz:02d}E{syd_tg:02d} "
-        else:
-            formatted_episode = f"E{syd_tg:02d} "
+       # if tg_Syd_Xyz == "True":
+        formatted_episode = f"S{syd_xyz:02d}E{syd_tg:02d} "
+       # else:
+          #  formatted_episode = f"E{syd_tg:02d} "
         Syd = formatted_episode + sydX
         mrsyds = ['YTS.MX', 'SH3LBY', 'Telly', 'Moviez', 'NazzY', 'VisTa', 'PiRO', 'PAHE', 'ink', 'mkvcinemas', 'CZ', 'WADU', 'PrimeFix', 'HDA', 'PSA', 'GalaxyRG', '-Bigil', 'TR', 'www.', '@',
             '-TR', '-SH3LBY', '-Telly', '-NazzY', '-PAHE', '-WADU', 'MoviezVerse', 't3nzin', '[Tips', 'Eac3', '(@'
