5b4f82d144b78d08f2ddfd942948516ef3198aaf
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-12-29 19:21:13 +0530

    Update mrsyd.py

--- plugins/mrsyd.py
+++ plugins/mrsyd.py
@@ -769,15 +769,19 @@ async def autosyd(client, file_details):
                 os.remove(ph_path)
             # Mark the file as ignored
             return await upload_msg.edit(f"Error: {e}")
-        mrsyddddd = newsydfil.document.file_size if type == "document" else newsydfil.video.file_size if type == "video" else newsydfil.audio.file_size
+        mrsydnew = newsydfil.document.file_size if type == "document" else newsydfil.video.file_size if type == "video" else newsydfil.audio.file_size
         os.remove(file_path)
         if ph_path:
             os.remove(ph_path)
         del renaming_operations[file_id]
         return await message.reply_text("ʀᴇᴜᴩʟᴏᴀᴅᴇᴅ")
 
-    if mrsyyd != mrsyddddd:
-        return await message.reply_text("ꜱɪᴢᴇ ᴍɪꜱᴍᴀᴛᴄʜ ᴀꜰᴛᴇʀ ꜱᴇᴄᴏɴᴅ ᴛʀʏ")
+        if mrsyyd != mrsydnew:
+            if ph_path:
+                os.remove(ph_path)
+            if metadata_path:
+                os.remove(metadata_path)
+            return await message.reply_text("ꜱɪᴢᴇ ᴍɪꜱᴍᴀᴛᴄʜ ᴀꜰᴛᴇʀ ꜱᴇᴄᴏɴᴅ ᴛʀʏ")
     if ph_path:
         os.remove(ph_path)
     if metadata_path:
