a785407634e3ecddaa428365f00ea14a1cd32f15
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-01-04 06:45:39 +0530

    Update syd.py

--- plugins/syd.py
+++ plugins/syd.py
@@ -63,25 +63,28 @@ async def start_processing(client, message):
         async for msg in client.get_chat_history(chat_id):  # Fetch in chronological order
             message_ids.append(msg.id)
 
+        await message.reply_text("1")
         message_ids.reverse()
+        await message.reply_text("jm1")
 
         print(f"Collected {len(message_ids)} message IDs for chat ID: {chat_id}")
 
         # Process each message ID one by one
         for message_id in message_ids:
-            await process_existing_messages(client, chat_id, message_id)
+            await process_existing_messages(client, chat_id, message_id, message)
 
         print("All messages processed.")
     except Exception as e:
         logger.error(f"An error occurred in start_processing: {e}")
         await message.reply_text("An error occurred while starting the processing.")
 
-async def process_existing_messages(client, chat_id, message_id):
+async def process_existing_messages(client, chat_id, message_id, syd):
     global mrsydt_g
     try:
         # Fetch the message by ID
+        await syd.reply_text("1bn")
         message = await client.get_messages(chat_id=chat_id, message_ids=message_id)
-        
+        await syd.reply_text("1")
         if message.media:
             file = getattr(message, message.media.value, None)
             if file and file.file_size > 10 * 1024 * 1024:  # > 10 MB
