20c20d7e7b7405ac359c0d6594795682120bb691
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-01-09 18:24:36 +0530

    Update syd.py

--- plugins/syd.py
+++ plugins/syd.py
@@ -304,10 +304,10 @@ async def autosyd(client, file_details):
             return  # Exit the handler if the file is being ignored
     renaming_operations[file_id] = datetime.now()
     episode_number = extract_episode_number(file_name)
-    qualit = extract_quality(file_name) if extract_quality(file_name) else '4k'
+    qualit = extract_quality(file_name) or '4k'
     if qualit == "2160p":
         return
-    season_no = extract_season_number(file_name) if extract_season_number(file_name) else '01'
+    season_no = extract_season_number(file_name) or '01'
     print(f"Extracted Episode Number: {episode_number}")
     
     if episode_number and season_no:
