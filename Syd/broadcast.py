01adc586f50b7d3497f3cf8488ab64bb4aeb15dc
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-01-18 17:42:28 +0530

    Update broadcast.py

--- Syd/broadcast.py
+++ Syd/broadcast.py
@@ -70,7 +70,7 @@ async def verupikls(bot, message):
 
     success = 0
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        pti, sh = await broadcast_messages(int(user['_id']), b_msg)
         if pti:
             success += 1
         elif pti == False:
