5a46ad128f6c9334480dace005b35282022bb13e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-01-18 08:42:11 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,1 +1,11 @@
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
+@Client.on_message(filters.command("clone") & filters.user(ADMINS))
+async def clone_menu(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The ᴅᴜᴍᴩ ᴄʜᴀɴɴᴇʟ ɪᴅ__\n\nExᴀᴍᴩʟᴇ:- `/set_dump -1002042969565`**")
+    mrsyd = message.text.split(" ", 1)[1]
+    syd = Client(
+        f"{bot_token}", API_ID, API_HASH,
+        bot_token=mrsyd,
+        plugins={"root": "Syd"}
+    )
+    await syd.start()
