8fa2a56758a318f4d61e60a89327be199019ab4a
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-04 18:45:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
+    START_PIC   = os.environ.get("START_PIC", "https://envs.sh/cKM.jpg")  
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "GetTGLinks") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
